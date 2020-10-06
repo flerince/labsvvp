04f0f42d3ba52f0d19f65d4afb88dedcf0e53029
Alona <alena.lovel.milih@gmail.com>
2020-10-06 15:57:02 +0300

    new

---
+++ laba4/zad5/zad5.cpp
@@ -0,0 +1,20 @@
+﻿#include <stdio.h>
+#include <locale.h>
+#include <math.h> 
+
+main()
+{
+	setlocale(LC_ALL, "Russian");
+	float a, b;
+	printf("Введите два числа \n");
+	scanf_s("%f%f", &a, &b);
+	if (a != 0 && b != 0) {
+		printf("Сумма модулей чисел: %f \n", fabs(a) + fabs(b));
+		printf("Разность модулей чисел: %f \n", fabs(a) - fabs(b));
+		printf("Произведение модулей чисел: %f \n", fabs(a) * fabs(b));
+		printf("Частное модулей чисел: %f \n", fabs(a) / fabs(b));
+	}
+	else {
+		printf("В одно или в оба числа введены нулевые значения");
+	}
+}
