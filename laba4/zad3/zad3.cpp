1b24222fa99546a724d02ce5829fc9fdb78c539d
Alona <alena.lovel.milih@gmail.com>
2020-10-06 15:55:09 +0300

    new

---
+++ laba4/zad3/zad3.cpp
@@ -0,0 +1,10 @@
+﻿#include <stdio.h>
+#include <locale.h>
+main()
+{
+	setlocale(LC_ALL, "Russian");
+	float a, b;
+	printf("Введите два числа \n");
+	scanf_s("%f%f", &a, &b);
+	printf("Среднее арифметическое чисел: %f \n", (a + b) / 2);
+}
