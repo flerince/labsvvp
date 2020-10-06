16714711d9dc852116ac69b91ec6b9c615860a92
Alona <alena.lovel.milih@gmail.com>
2020-10-06 15:52:33 +0300

    new

---
+++ laba4/zad1/zad1.cpp
@@ -0,0 +1,13 @@
+﻿#include <stdio.h>
+#include <locale.h>
+main()
+{
+	setlocale(LC_ALL, "Russian");
+	float a, b, S, P;
+	printf("Введите два числа \n");
+	scanf_s("%f%f", &a, &b);
+	S = a * b;
+	P = (a + b) * 2;
+	printf("Площадь: %f \n", S);
+	printf("Периметр: %f \n", P);
+}
