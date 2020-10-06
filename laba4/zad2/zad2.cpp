6af61e9e0d263efeb1f85f086cfb8e2fa2119846
Alona <alena.lovel.milih@gmail.com>
2020-10-06 15:54:19 +0300

    new

---
+++ laba4/zad2/zad2.cpp
@@ -0,0 +1,10 @@
+﻿#include <stdio.h>
+#include <locale.h>
+main()
+{
+	setlocale(LC_ALL, "Russian");
+	float d;
+	printf("Введите диаметр окружности \n");
+	scanf_s("%f", &d);
+	printf("Длина окружности: %f \n", d * 3.14);
+}
