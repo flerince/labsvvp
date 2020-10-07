63a0293422d8a81f29ea41a20ab84a401c46ec06
Alona <alena.lovel.milih@gmail.com>
2020-10-07 13:35:45 +0300

    new

---
+++ laba6/n1/n1.cpp
@@ -0,0 +1,13 @@
+﻿#include <stdio.h>
+#include <locale.h>
+main()
+{
+	setlocale(LC_ALL, "Russian");
+	int a,b;
+	printf("Введите значения переменных a и b через пробел \n");
+	scanf_s("%d%d", &a, &b);
+	a = a + b;
+	b = a - b;
+	a = a - b; 
+	printf("Поменяем значения переменных местами, a=%d, b=%d \n", a, b); // Выводим нужный ответ
+}
