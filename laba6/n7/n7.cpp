b7044afe220f50194fc91efb424081cf4e5b799e
Alona <alena.lovel.milih@gmail.com>
2020-10-07 14:32:23 +0300

    new

---
+++ laba6/n7/n7.cpp
@@ -0,0 +1,15 @@
+﻿#include <stdio.h>
+#include <locale.h>
+#include <math.h>
+main()
+{
+	setlocale(LC_ALL, "Russian");
+	float A, vsp1, vsp2;
+	printf("Введите значение A \n");
+	scanf_s("%f", &A);
+	vsp1 = A * A; // 2 степень
+	vsp2 = vsp1 * vsp1; // 4 
+	vsp2 = vsp2 * A; // 5
+	vsp2 = vsp2 * vsp2 * vsp2;
+	printf("A в 15 степени = %f \n", vsp2);
+}
