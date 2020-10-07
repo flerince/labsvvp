137ea0cdbd34668bb0c6999b09c6b46f29d0538b
Alona <alena.lovel.milih@gmail.com>
2020-10-07 14:11:05 +0300

    new

---
+++ laba6/n5/n5.cpp
@@ -0,0 +1,13 @@
+﻿#include <stdio.h>
+#include <locale.h>
+#include <math.h>
+main()
+{
+	setlocale(LC_ALL, "Russian");
+	float x, y,l;
+	printf("Дано значение x = 4 \n");
+	x = 4;
+	x = x - 3; 
+	y = 4 * pow(x, 6) - 7 * pow(x, 2) + 2;
+	printf("При данном значении x значение функции y = 4*(x-3)^6-7*(x-3)^2+2 = %f \n", y);
+}
