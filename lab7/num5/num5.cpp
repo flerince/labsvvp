8aae12b91ac4c710b8e54a015684119e73f4eef6
Alona <alena.lovel.milih@gmail.com>
2020-10-25 16:45:18 +0300

    NEW

---
+++ lab7/num5/num5.cpp
@@ -0,0 +1,14 @@
+﻿#include <stdio.h>
+#include <locale.h>
+main()
+{
+	setlocale(LC_ALL, "Russian");
+	float A,B,x;
+	printf("Введите значение коэффицентов A (не равное 0) и B для уравнения A*x+B=0 \n");
+	scanf_s("%f%f", &A, &B);
+	if (A != 0) {
+		x = -B / A;
+		printf("Х в уравнении = %5.3f \n", x);
+	}
+	else printf("Введено неверное значение A \n");
+}
