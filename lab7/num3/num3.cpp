3e8b1764a516f7c855d65594cd1e7ebda80952ac
Alona <alena.lovel.milih@gmail.com>
2020-10-25 16:26:53 +0300

    new

---
+++ lab7/num3/num3.cpp
@@ -0,0 +1,12 @@
+﻿#include <stdio.h>
+#include <locale.h>
+main()
+{
+	setlocale(LC_ALL, "Russian");
+	float A, X, Y, price;
+	printf("Введите значение X - кг конфет, A - их стоимость, Y - новое значение кг конфет \n");
+	scanf_s("%f%f%f", &X, &A ,&Y);
+	price = A / X;
+	printf("Цена за 1 кг %5.2f, цена за Y кг - %5.2f \n", price, price*Y);
+
+}
