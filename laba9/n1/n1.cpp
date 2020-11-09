362a04157989648f9e544e9d9ef6bf3e62d50031
Alona <alena.lovel.milih@gmail.com>
2020-11-09 21:51:55 +0300

    new

---
+++ laba9/n1/n1.cpp
@@ -0,0 +1,13 @@
+﻿#include <stdio.h>
+#include <locale.h>
+#include <math.h>
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+	int N,s;
+	printf("Введите N - количество секунд с начала суток \n");
+	scanf_s("%d", &N);
+	s = N - 60 * trunc(N / 60);
+	printf("Количество секунд, прошедших с начала последней минуты: %d", s);
+	return 0;
+}
