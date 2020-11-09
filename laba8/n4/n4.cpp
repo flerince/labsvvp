8a29f19ef3f09971044bef466e023c44e7fd3dc4
Alona <alena.lovel.milih@gmail.com>
2020-11-09 21:32:08 +0300

    new

---
+++ laba8/n4/n4.cpp
@@ -0,0 +1,18 @@
+﻿#include <stdio.h>
+#include <locale.h>
+#include <math.h>
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+	int A,s,a1,a2;
+	printf("Введите целое двузначное число \n");
+	scanf_s("%d", &A);
+	if (A>99 or A<10)
+		printf("Введеное значение A не соответствует условию");
+	else {
+		a1 = A / 10; 
+		a2 = A % 10;
+		printf("Число наоборот %d", a2*10+a1);
+	}
+	return 0;
+}
