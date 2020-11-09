076f82102cf4dec6fe682bf982e3062c1c906d20
Alona <alena.lovel.milih@gmail.com>
2020-11-09 19:59:39 +0300

    new

---
+++ laba8/n1/n1.cpp
@@ -0,0 +1,12 @@
+﻿#include <stdio.h>
+#include <locale.h>
+#include <math.h>
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+	int a;
+	printf("Введите размер файла в байтах \n"); 
+	scanf_s("%d", &a);
+	printf("Размер файла в килобайтах (целое значение): %1.0f \n", trunc (a/1024) );
+	return 0;
+}
