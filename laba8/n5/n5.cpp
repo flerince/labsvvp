64aed147ce75333f5b2e755d4b1314215acdebde
Alona <alena.lovel.milih@gmail.com>
2020-11-09 21:34:24 +0300

    new

---
+++ laba8/n5/n5.cpp
@@ -0,0 +1,18 @@
+﻿#include <stdio.h>
+#include <locale.h>
+#include <math.h>
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+	int A, s, a1, a2;
+	printf("Введите целое трехзначное число \n");
+	scanf_s("%d", &A);
+	if (A > 999 or A < 100)
+		printf("Введеное значение A не соответствует условию");
+	else {
+		a1 = A / 100;
+		a2 = A % 100;
+		printf("Число наоборот %d", a2 * 10 + a1);
+	}
+	return 0;
+}
