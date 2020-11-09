db4166b936eed862e5125aac1fe3b379a27bde0f
Alona <alena.lovel.milih@gmail.com>
2020-11-09 20:06:29 +0300

    new

---
+++ laba8/n2/n2.cpp
@@ -0,0 +1,12 @@
+﻿#include <stdio.h>
+#include <locale.h>
+#include <math.h>
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+	int A,B;
+	printf("Введите длины отрезков А и В \n");
+	scanf_s("%d%d", &A, &B);
+	printf("Отрезок B может поместиться в отрезок A следующее количество раз: %1.0f (без наложений)\n", trunc(A/B));
+	return 0;
+}
