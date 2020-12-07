5bc1094eb80102d97bbde3a0ccfdc321235ac9ed
Alona <alena.lovel.milih@gmail.com>
2020-12-07 13:07:21 +0400

    new

---
+++ laba10/n3/n3.cpp
@@ -0,0 +1,17 @@
+﻿#include <stdio.h>
+#include <locale.h>
+
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+	int A;
+	printf("Введите целое положительное число A\n");
+	scanf_s("%d", &A);
+	if (A >= 0) {
+		if (A > 9 && A < 100 && (A % 2) == 0)
+			printf("Выражение «Данное число является четным двузначным» истино");
+		else printf("Выражение «Данное число является четным двузначным» ложно");
+	}
+	else printf("Введено отрицательное число");
+	return 0;
+}
