2a2e92bded4bf6fd28b01849c5b1fa7e4faef956
Alona <alena.lovel.milih@gmail.com>
2020-12-07 15:27:01 +0400

    new

---
+++ laba11/n4/n4.cpp
@@ -0,0 +1,17 @@
+﻿#include <stdio.h>
+#include <locale.h>
+#include <math.h>
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+	int x,y;
+	printf("Введите координаты точки (х, у) не лежащей на координатных осях \n");
+	scanf_s("%d%d", &x, &y);
+	if (x > 0) {
+		if (y > 0) printf("Точка находится в 1 координатной четверти");
+		else printf("Точка находится в 4 координатной четверти");
+	}
+	else if (y > 0) printf("Точка находится в 2 координатной четверти");
+	else printf("Точка находится в 3 координатной четверти");
+	return 0;
+}
