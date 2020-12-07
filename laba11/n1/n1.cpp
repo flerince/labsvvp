9c0ab4a32bb6f36fbb93f50c78256825f824950e
Alona <alena.lovel.milih@gmail.com>
2020-12-07 14:48:31 +0400

    new

---
+++ laba11/n1/n1.cpp
@@ -0,0 +1,19 @@
+﻿#include <stdio.h>
+#include <locale.h>
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+	int A, B;
+	printf("Введите значение А и В \n");
+	scanf_s("%d%d", &A, &B);
+	if (A == B) {
+		A=0;
+		B=0;
+	}
+	else {
+		if (A > B) B = A;
+		else A = B;
+	}
+	printf("Новые значения переменных: А=%d, B=%d", A, B);
+	return 0;
+}
