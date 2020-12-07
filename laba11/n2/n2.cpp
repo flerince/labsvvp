5856762dab9cd2bf7347e649f0426931cde7fe83
Alona <alena.lovel.milih@gmail.com>
2020-12-07 14:59:15 +0400

    new

---
+++ laba11/n2/n2.cpp
@@ -0,0 +1,21 @@
+﻿#include <stdio.h>
+#include <locale.h>
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+	int A, B, C, max, min, x;
+	printf("Введите значение А В C \n");
+	scanf_s("%d%d%d", &A, &B, &C);
+	if (A > B) {
+		max = A;
+		min = B;
+	}
+	else {max = B;
+	min = A;
+}
+	if (max < C) x = max + C;
+	else if (min < C) x = max + C;
+	else x = max + min;
+	printf("Сумма двух наибольших чисел = %d", x);
+	return 0;
+}
