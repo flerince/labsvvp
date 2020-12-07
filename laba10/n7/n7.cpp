4c650788d12557ef35ea2053af639b32fa62c444
Alona <alena.lovel.milih@gmail.com>
2020-12-07 13:39:31 +0400

    new

---
+++ laba10/n7/n7.cpp
@@ -0,0 +1,13 @@
+﻿#include <stdio.h>
+#include <locale.h>
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+	int a, b, c;
+	printf("Введите стороны треугольника a,b,c \n");
+	scanf_s("%d%d%d", &a, &b, &c);
+	if ((a<(b+c)) && (b<(a+c)) && (c<(a+b)))
+		printf("Выражение «Существует треугольник со сторонами a, b, c» истинно");
+	else printf("Выражение «Существует треугольник со сторонами a, b, c» ложно");
+	return 0;
+}
