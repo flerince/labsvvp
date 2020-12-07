6f15d247b9bb5f71dcdc11046463bd2b8e106ba9
Alona <alena.lovel.milih@gmail.com>
2020-12-07 12:17:51 +0400

    new

---
+++ laba9/n4/n4.cpp
@@ -0,0 +1,14 @@
+﻿#include <stdio.h>
+#include <locale.h>
+#include <math.h>
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    int A,B,C,S,n;
+    printf("Введите длины прямоугольника AxB и квадрат со стороной С \n");
+    scanf_s("%d%d%d", &A, &B, &C);
+    n = (A / C) * (B/C);
+    S = A * B - n * C * C;
+    printf("Квадрат поместится в прямоугольник %d раз, останется %d свободной площади", n,S);
+    return 0;
+}
