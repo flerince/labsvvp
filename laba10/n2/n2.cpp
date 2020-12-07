4208b0d39f1fde936056a3b2230e5eeec80a8d8c
Alona <alena.lovel.milih@gmail.com>
2020-12-07 13:02:19 +0400

    new

---
+++ laba10/n2/n2.cpp
@@ -0,0 +1,14 @@
+﻿#include <stdio.h>
+#include <locale.h>
+
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+	int A, B, C;
+	printf("Введите числа A B C \n");
+	scanf_s("%d%d%d", &A, &B, &C);
+	if (A < B && B < C)
+		printf("Выражение A < B < C истино");
+	else printf("Выражение A < B < C ложно");
+	return 0;
+}
