738f88ef29c42dea3ef808e072ba4f011d7d8725
Alona <alena.lovel.milih@gmail.com>
2020-12-07 12:28:19 +0400

    new

---
+++ n5/n5.cpp
@@ -0,0 +1,13 @@
+﻿#include <stdio.h>
+#include <locale.h>
+#include <math.h>
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    int N, a;
+    printf("Введите год N \n");
+    scanf_s("%d", &N);
+    a = ((N - 1) / 100) + 1;
+    printf("Вы ввели год %d столетия", a);
+    return 0;
+}
