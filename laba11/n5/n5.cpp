f552852ef35ae2fdf3eef1c148038a128b1352c9
Alona <alena.lovel.milih@gmail.com>
2020-12-07 15:32:24 +0400

    new

---
+++ laba11/n5/n5.cpp
@@ -0,0 +1,19 @@
+﻿#include <stdio.h>
+#include <locale.h>
+#include <math.h>
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+	int x;
+	printf("Введите число х \n");
+	scanf_s("%d", &x);
+	if (x != 0) {
+		if (x < 0) {
+			if (x % 2 == 0) printf("Вы ввели отрицательное четное число \n");
+			else printf("Вы ввели отрицательное нечетное число \n");
+		}
+		else if (x % 2 == 0) printf("Вы ввели положительное четное число \n");
+		else printf("Вы ввели положительное нечетное число \n");
+	}
+	else printf("Вы ввели нулевое число \n");
+}
