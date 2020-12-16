902813e6a23f21e99fe40f4bc5c681525de00551
Alona <alena.lovel.milih@gmail.com>
2020-12-16 14:54:23 +0400

    new

---
+++ laba13/n1/n1.cpp
@@ -0,0 +1,14 @@
+﻿#include <stdio.h>
+#include <locale.h>
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+	float c=0.0f, N, i;
+	printf("Введите стоимость кг конфет в руб.\n");
+	scanf_s("%f", &N);
+	for (i = 0.1; i < 1.1; i = i + 0.1) {
+		c = N * i;
+		printf("Стоимость за %4.1f кг = %4.1f руб. \n", i, c);
+	}
+	return 0;
+}
