d841df4a6bc2dc3916476d1d43f5243829ae9fc6
Alona <alena.lovel.milih@gmail.com>
2020-12-16 15:33:38 +0400

    new

---
+++ laba13/n4/n4.cpp
@@ -0,0 +1,16 @@
+﻿#include <stdio.h>
+#include <locale.h>
+#include <math.h>
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+	int c = 1, N, i,A, a=0;
+	printf("Введите число А и его максимальную степень N \n");
+	scanf_s("%d%d", &A,&N);
+	for (i = 1; i < (pow(A,N)); i = i*A) {
+		c = c + i;
+		a = a + c;
+	}
+	printf("Значение суммы 1+А+А^2+...+A^N = %d \n", a);
+	return 0;
+}
