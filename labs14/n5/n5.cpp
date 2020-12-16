5960ef4ff81bbcd66051234bde785caec5b68ae1
Alona <alena.lovel.milih@gmail.com>
2020-12-16 17:56:45 +0400

    new

---
+++ labs14/n5/n5.cpp
@@ -0,0 +1,17 @@
+﻿#include <stdio.h>
+#include <locale.h>
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+	int A,B;
+	printf("Введите числа А и В\n");
+	scanf_s("%d%d", &A,&B);
+	while (A != B) {
+		if (A > B)
+			A = A - B;
+		else
+			B = B - A;
+	}
+	printf("Их наибольший общий делитель: %d \n", A);
+  return 0;
+}
