b7aca5d65d5c1169581fb9a210e31c3d9ad55291
Alona <alena.lovel.milih@gmail.com>
2020-12-16 15:57:05 +0400

    NEW

---
+++ laba13/n5/n5.cpp
@@ -0,0 +1,16 @@
+﻿#include <stdio.h>
+#include <locale.h>
+#include <math.h>
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+	int c = 1, N, i, A, a = 1;
+	printf("Введите число А и его максимальную степень N \n");
+	scanf_s("%d%d", &A, &N);
+	for (i = 1; i < N+1; i++) {
+		c = c*A*(-1);
+		a = a + c;
+	}
+	printf("Значение выражения 1-А+А^2-...+-A^N = %d \n", a);
+	return 0;
+}
