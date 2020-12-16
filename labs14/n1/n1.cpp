a8fb58d3e84d45d0f1e1f4a26bf4a94a5a663573
Alona <alena.lovel.milih@gmail.com>
2020-12-16 17:02:18 +0400

    new

---
+++ labs14/n1/n1.cpp
@@ -0,0 +1,14 @@
+﻿#include <stdio.h>
+#include <locale.h>
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+	int A, B, i, j;
+	printf("Введите числа A и B (A<B)\n");
+	scanf_s("%d%d", &A, &B);
+	for (A; A < B + 1; A++) {
+		for (j = 1; j < A+1; j++)
+			printf("%d  \n", A);
+	}
+	return 0;
+}
