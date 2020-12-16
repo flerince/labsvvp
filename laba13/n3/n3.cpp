38ec45b9061a949b8a5379fc38958ee1a1545f4e
Alona <alena.lovel.milih@gmail.com>
2020-12-16 15:13:57 +0400

    new

---
+++ laba13/n3/n3.cpp
@@ -0,0 +1,15 @@
+﻿#include <stdio.h>
+#include <locale.h>
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+	int c = 0, N, i;
+	printf("Введите число, квадрат которого хотите посчитать\n");
+	scanf_s("%d", &N);
+	for (i = 1; i < (2*N) ; i = i + 2) {
+		c = c + i;
+		printf("Текущее значение суммы: %d \n", c);
+	}
+	printf("Итоговое значение: %d \n", c);
+	return 0;
+}
