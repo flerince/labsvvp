114ecfa82ada9c6c01e8631335586aaaf78c8ccc
Alona <alena.lovel.milih@gmail.com>
2020-12-16 17:07:34 +0400

    new

---
+++ labs14/n2/n2.cpp
@@ -0,0 +1,15 @@
+﻿#include <stdio.h>
+#include <locale.h>
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+	int A, B, i, j, count = 0;;
+	printf("Введите числа A и B (A>B)\n");
+	scanf_s("%d%d", &A, &B);
+	while (A >= B) {
+		count++;
+		A = A - B;
+	}
+	printf("B поместится в А ровно %d раз(а).\n", count);
+	return 0;
+}
