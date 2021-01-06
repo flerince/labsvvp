bac81d06ae83ce6495be128d8ef690dad130f74e
Alona <alena.lovel.milih@gmail.com>
2021-01-06 08:07:31 +0300

    new

---
+++ laba16/n4/n4.cpp
@@ -0,0 +1,23 @@
+﻿#include <stdio.h>
+#include <locale.h>
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+	int a[100];
+	int n, i;
+	printf("Введите длину массива N (N>0)\n");
+	scanf_s("%d", &n);
+	if (n > 0) {
+		for (i = 0; i < n; i++) {
+			printf("a[%i] : ", i);
+			scanf_s("%d", &a[i]);
+		}
+		for (i = 0; i < n / 2; i++) {
+			printf("%d \n", a[i]);
+			printf("%d \n", a[n - i - 1]);
+		}
+		if (n % 2 != 0) 
+			printf("%i\n", a[(n / 2)]);
+	}
+	return 0;
+}
