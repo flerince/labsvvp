b811efe979f19b9e6912f058122657caa03b6828
Alona <alena.lovel.milih@gmail.com>
2021-01-06 05:34:34 +0300

    new

---
+++ laba15/n2/n2.cpp
@@ -0,0 +1,19 @@
+﻿#include <stdio.h>
+#include <locale.h>
+int Sign(float X)
+{
+	if (X < 0) return -1;
+	if (X == 0) return 0;
+	if (X > 0) return 1;
+ }
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+	float A, B;
+	printf("Введите число A и B \n");
+		scanf_s("%f%f", &A, &B);
+		A = Sign(A);
+		B = Sign(B);
+		printf("%f \n", A+B);
+	return 0;
+}
