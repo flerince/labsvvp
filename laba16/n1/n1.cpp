be0a9e2f8c062678085e74e661a72d301c35e8cb
Alona <alena.lovel.milih@gmail.com>
2021-01-06 07:21:23 +0300

    new

---
+++ laba16/n1/n1.cpp
@@ -0,0 +1,17 @@
+﻿#include <stdio.h>
+#include <locale.h>
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+	int a[100];
+	int n,i,j=1;
+	printf("Введите число N (N>0) \n");
+	scanf_s("%d", &n);
+	if (n > 0) {
+		for (i = 0; i < n; i++, j = j + 2)
+			a[i] = j;
+		for (i = 0; i < n; i++)
+			printf("Элемент массива номер %d - %d \n",i,a[i]);
+	}
+	return 0;
+}
