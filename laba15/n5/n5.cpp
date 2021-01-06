5007bb1247b9d0f19e068478cae3eb642f114284
Alona <alena.lovel.milih@gmail.com>
2021-01-06 06:39:58 +0300

    new

---
+++ laba15/n5/n5.cpp
@@ -0,0 +1,22 @@
+﻿#include <stdio.h>
+#include <locale.h>
+float Fact2(int N)
+{
+	int S = 1;
+		while (N>0)
+		{
+			S = S * float(N);
+			N = N - 2;
+	}
+	return S;
+}
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+	int X,N;
+	printf("Введите число N (N>0) \n");
+		scanf_s("%d", &N);
+		X = Fact2(N);
+        printf("N!! = %d , ", X);
+	return 0;
+}
