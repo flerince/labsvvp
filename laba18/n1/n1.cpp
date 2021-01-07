7800da859210b8ff9ccbb8de7ccaa60742035042
Alona <alena.lovel.milih@gmail.com>
2021-01-07 23:41:47 +0300

    new

--- laba18/n1/n1.cpp
+++ laba18/n1/n1.cpp
@@ -4,19 +4,15 @@
 int main() {
 	setlocale(LC_ALL, "Russian");
 	const int n = 5;
-	int a[n], b[n], c[n],i;
-	printf("Введите значения массива а \n");
+	int a[n], b[n], c[n], i;
+	printf("Введите сначала значения массива а, затем значения массива б \n");
 	for (i = 0; i < n; i++) {
 		printf("a[%d] : ", i);
 		scanf_s("%d", &a[i]);
 	}
-	printf("Введите значения массива b \n");
 	for (i = 0; i < n; i++) {
 		printf("b[%d] : ", i);
 		scanf_s("%d", &b[i]);
-	}
-	for (i = 0; i < n; i++) {
-		c[i] = a[i];
 	}
 	for (i = 0; i < n; i++) {
 		c[i] = a[i];
@@ -27,13 +23,13 @@ int main() {
 	for (i = 0; i < n; i++) {
 		b[i] = c[i];
 	}
-	printf("Новые значения массива а \n");
+	printf("Новые значения массива а: \n");
 	for (i = 0; i < n; i++) {
-		printf("a[%d] : %d \n", i, a[i]);
+		printf("a[%d] = %d \n", i, a[i]);
 	}
-	printf("Новые значения массива b \n");
+	printf("Введите сначала значения массива б \n");
 	for (i = 0; i < n; i++) {
-		printf("b[%d] : %d \n", i, b[i]);
+		printf("b[%d] = %d \n", i, b[i]);
 	}
 	return 0;
 }
