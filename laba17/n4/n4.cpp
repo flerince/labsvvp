8d980b19ff500a846e8f87f8cbe4d4b18ad7764b
Alona <alena.lovel.milih@gmail.com>
2021-01-07 11:03:15 +0300

    new

---
+++ laba17/n4/n4.cpp
@@ -0,0 +1,20 @@
+﻿#include <stdio.h>
+#include <locale.h>
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+	const int n = 10;
+	int a[n], max, i, num = 0;
+	for (i = 0; i < n; i++) {
+		printf("a[%d] : ", i);
+		scanf_s("%d", &a[i]);
+	}
+	max = a[0];
+	for (i = 1; i < n; i++)
+		if ((a[i]>a[i+1]) && (a[i]>a[i-1])) {
+			max = a[i];
+			num = i;
+		}
+	printf("Последний максимальный элемент среди соседей: a[%d]=%d", num, max);
+	return 0;
+}
